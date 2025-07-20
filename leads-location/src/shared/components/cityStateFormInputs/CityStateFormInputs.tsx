46a154c7960f155cd071985533428e73d4ca4954
Matheus Zauza Maschietto <matheus_maschietto@yahoo.com.br>
2025-07-20 11:33:43 -0300

    Refactoring components

--- leads-location/src/shared/components/cityStateFormInputs/CityStateFormInputs.tsx
+++ leads-location/src/shared/components/cityStateFormInputs/CityStateFormInputs.tsx
@@ -1,50 +1,24 @@
 "use client"
 
-import React, { useEffect, useState } from 'react'
-import { set } from 'react-hook-form';
+import React, { useState } from 'react'
 
 type Props = {}
 
 function CityStateFormInputs({}: Props) {
-    const [form, setForm] = useState({ state: '', city: '', cep: '', cepNotFound: false })
-    const [cities, setCities] = useState<{ id: number, name: string }[]>([]);
-    
-    useEffect(() => {
-        if (form.state) {
-            getCities(form.state).then(cities => {
-                setCities(cities.map((city: { id: number, nome: string }) => ({
-                    id: city.id,
-                    name: city.nome
-                })));
-            });
-        }
-    }, [form.state, form.cep]);
+    const [cep, setCep] = useState({cepValue: '', cepNotFound: false});
+    const [state, setState] = useState('');
+    const [city, setCity] = useState({ city: '', cities: [] });
 
-    useEffect(() => {
-        if (form.cep) {
-            setCep(form.cep);
-        }
-    }, [form.cep])
-
-    async function setCep(cep: string) {
+    async function getCep(cep: string) {
         try {
             const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
             const data = await response.json()
             if (data.erro) {
                 throw new Error('CEP inválido')
             }
-            setForm(prev => ({
-                ...prev,
-                state: data.uf,
-                city: data.ibge,
-                cepNotFound: false
-            }))
+            return data;
         } catch (error) {
             console.error('Erro ao buscar CEP:', error)
-            setForm(prev => ({
-                ...prev,
-                cepNotFound: true,
-            }))
         }
     }
 
@@ -52,29 +26,54 @@ function CityStateFormInputs({}: Props) {
         try {
             const response = await fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${state}/municipios`)
             const data = await response.json()
-            return data.map((municipio: { id:number, nome: string }) => ({ id: municipio.id, nome: municipio.nome }))
+            return data.map((city: { id:number, nome: string }) => ({ id: city.id, name: city.nome }))
         } catch (error) {
-            console.error('Erro ao buscar cidades:', error)
             return []
         }
     }
 
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
-        const { name, value } = e.target;
-        setForm(prev => ({
-            ...prev,
-            [name]: value,
-        }));
-    };
+    async function handleStateChange(e: React.ChangeEvent<HTMLSelectElement>) {
+        const value = e.target.value;
+        setState(value);
+        const cities = await getCities(value)
+        
+        setCity({ ...city, cities: cities }); 
+    }
+
+    async function handleCityChange(e: React.ChangeEvent<HTMLSelectElement>) {
+        const value = e.target.value;
+        setCity({ ...city, city: value });
+    }
+
+    async function handleCEPBlur(e: React.ChangeEvent<HTMLInputElement>) {
+        const value = e.target.value.replace(/\D/g, ''); 
+        try{
+            const cepInformations = await getCep(value)
+
+            if (!cepInformations) return;
+    
+            setCep({ cepValue: value, cepNotFound: false });
+            setState(cepInformations.uf);
+            setCity({ city: cepInformations.ibge, cities: await getCities(cepInformations.uf) });
+        }
+        catch (error) {
+            setCep({ ...cep, cepNotFound: true });
+        }
+    }
+
+    async function handleCEPChange(e: React.ChangeEvent<HTMLInputElement>) {
+        const value = e.target.value.replace(/\D/g, ''); 
+        setCep({ cepValue: value, cepNotFound: false });
+    }
 
   return (
     <>
         <label className='block mb-2 text-sm font-medium text-gray-700'>CEP *
-            <input type="cep" name="cep" id="cep" className='w-full p-2 border border-gray-300 rounded-md' value={form.cep} onChange={handleChange} />
-            {form.cepNotFound && <span className='text-red-500 text-sm'>CEP não encontrado</span>}
+            <input type="cep" name="cep" id="cep" className='w-full p-2 border border-gray-300 rounded-md' value={cep.cepValue} onChange={handleCEPChange} onBlur={handleCEPBlur} />
+            {cep.cepNotFound && <span className='text-red-500 text-sm'>CEP não encontrado</span>}
         </label>
         <label className='block mb-2 text-sm font-medium text-gray-700'>Estado *
-            <select name="state" id="state" className='w-full p-2 border border-gray-300 rounded-md' value={form.state} onChange={handleChange}>
+            <select name="state" id="state" className='w-full p-2 border border-gray-300 rounded-md' value={state} onChange={handleStateChange}>
                 <option value="">Selecione um estado</option>
                 <option value="RO">Rondônia</option>
                 <option value="AC">Acre</option>
@@ -106,18 +105,17 @@ function CityStateFormInputs({}: Props) {
             </select>
         </label>
         <label className='block mb-2 text-sm font-medium text-gray-700'>Cidade *
-            <select name="city" id="city" className='w-full p-2 border border-gray-300 rounded-md' value={form.city} onChange={handleChange}>
+            <select name="city" id="city" className='w-full p-2 border border-gray-300 rounded-md' value={city.city} onChange={handleCityChange}>
                 {
-                    cities.length === 0 ?
-                    (
-                        <option value="">Selecione um estado primeiro</option>
-                    ) 
+                    city.cities.length === 0 ?
+                    (<option value="">Selecione um estado primeiro</option>) 
                     : 
-                    (
-                        cities.map(
-                            (city: { id: number, name: string }, id: number) => <option key={id} value={city.id}>{city.name}</option>
-                        )
-                    )
+                    ([
+                            <option key="default" value="">Selecione uma cidade</option>,   
+                            city.cities.map(
+                                (city: { id: number, name: string }, id: number) => <option key={id} value={city.id}>{city.name}</option>
+                            )
+                    ])
                 }
             </select>
         </label>
@@ -126,3 +124,4 @@ function CityStateFormInputs({}: Props) {
 }
 
 export default CityStateFormInputs
+
